8f78b6259857303b348e1783e238045b1cac8a1b
rndev2017 <30762323+rndev2017@users.noreply.github.com>
2019-11-02 23:42:22 -0400

    Update model.py

--- src/model.py
+++ src/model.py
@@ -7,16 +7,18 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, matthews_corrcoef
 
-
+# Reads data file provided in the data directory of this repo
 data = pd.read_csv("\\path\\to\\data")
 
+# Seperates the data set into input & output
 X = data.iloc[:, 0:4]
 Y = data.iloc[:, -1]
 
+# Seperates X & Y into test and validation sets
 train_X, test_X, train_Y, test_Y = train_test_split(
     X, Y, test_size=0.1, shuffle=True, stratify=Y, random_state=40)
 
-# model
+# Constructs xgboost classifier for multi-class classification
 model = xgb.XGBClassifier(objective='multi:softmax',
                           learning_rate=0.2,
                           gamma=4,
@@ -27,78 +29,94 @@ model = xgb.XGBClassifier(objective='multi:softmax',
                           silent=0,
                           subsample=0.8)
 eval_set = [(train_X, train_Y), (test_X, test_Y)]
+
+# Fits the model on the training set
 model.fit(train_X, train_Y.values.ravel(), early_stopping_rounds=5000,
           eval_metric=["merror"], eval_set=eval_set, verbose=True)
 
 
-# evaluating model on the test set
-predictions = model.predict(test_X)
-test_y_arr = np.array(test_Y)
+def calculate_accuracy(y_true, y_pred):
+    """Calculates the accuracy of the model
 
-correctpred, total = 0, 0
+       Arguments:
+           y_true {numpy.array} -- the true labels corresponding to each input
+           y_pred {numpy.array} -- the model's predictions
 
-for index in range(len(predictions)):
-    if(predictions[index] == test_y_arr[index]):
-        correctpred = correctpred + 1
-    total = total+1
+       Returns:
+           accuracy {str} -- the accuracy of the model (# out of 100)
+    """
+    correctpred, total = 0, 0
 
-accuracy = 'accuracy='+str((correctpred*100)/total))
-cm = confusion_matrix(test_y_arr, predictions)
+    for index in range(len(y_pred)):
+        if(y_pred[index] == y_true[index]):
+            correctpred = correctpred + 1
+        total = total+1
 
-# calculating recall of model on entire data set evaluation
-test_preds = predictions
-test_labels = test_y_arr
-
-recall_per_class, classes = [], []
-label_map = ['0', '1', '2']
+    return 'accuracy='+str((correctpred*100)/total))
 
 
-for target_label in np.unique(test_labels):
-    recall_numerator = np.logical_and(
-        test_preds == target_label, test_labels == target_label).sum()
-    recall_denominator = (test_labels == target_label).sum()
-    recall_per_class.append(recall_numerator / recall_denominator)
-    classes.append(label_map[target_label])
-recall = pd.DataFrame({'recall': recall_per_class, 'class_label': classes})
-recall.sort_values('class_label', ascending=False, inplace=True)
+def calculate_recall(y_true, y_pred):
+    """Calculates the model's recall on all three classes
 
+       Arguments:
+           y_true {numpy.array} -- the true labels corresponding to each input
+           y_pred {numpy.array} -- the model's predictions
 
-# accuracy for entire dataset
-correctpred_all, total_all = 0, 0
+       Returns:
+           recall {pandas.DataFrame} -- a DataFrame that contains the
+                                        recall of the model
+    """
+    recall_per_class, classes = [], []
+    label_map = ['0', '1', '2']
 
-predictions_all = model.predict(data.iloc[:, 0:4])
+    for target_label in np.unique(y_true):
+        recall_numerator = np.logical_and(
+            y_pred == target_label, y_true == target_label).sum()
+        recall_denominator = (y_true == target_label).sum()
+        recall_per_class.append(recall_numerator / recall_denominator)
+        classes.append(label_map[target_label])
+    recall = pd.DataFrame({'recall': recall_per_class, 'class_label': classes})
+    recall.sort_values('class_label', ascending=False, inplace=True)
 
-all_y = np.array(data.iloc[:, -1])
+    return recall
 
-for index in range(len(predictions_all)):
-    if(predictions_all[index] == all_y[index]):
-        correctpred_all = correctpred_all + 1
-    total_all = total_all+1
+def generate_classification_report(y_true, y_pred):
+    """Using sklearn to build a text report showing the main classification
+       metrics
 
-accuracy_all = 'accuracy='+str((correctpred_all*100)/total_all))
+       Arguments:
+           y_true {numpy.array} -- the true labels corresponding to each input
+           y_pred {numpy.array} -- the model's predictions
 
+       Returns:
+           report {str} -- contains the F1-score, precision, recall, & support
+                           of each class
 
-# calculating recall of model on entire data set evaluation
-test_preds_all = predictions_all
-test_labels_all = all_y
+    """
+    return classification_report(y_true, y_pred)
 
-recall_per_class_all, classes_all = [], []
+def calculate_confusion_matrix(y_true, y_pred):
+    """Using sklearn to compute a confusion matrix to evaluate
+       the accuracy of a classification
 
-for target_label in np.unique(test_labels_all):
-    recall_numerator_all = np.logical_and(
-        test_preds_all == target_label, test_labels_all == target_label).sum()
-    recall_denominator_all = (test_labels_all == target_label).sum()
-    recall_per_class_all.append(recall_numerator_all / recall_denominator_all)
-    classes_all.append(label_map[target_label])
-recall_all = pd.DataFrame(
-    {'recall': recall_per_class_all, 'class_label': classes_all})
-recall_all.sort_values('class_label', ascending=False, inplace=True)
+       Arguments:
+           y_true {numpy.array} -- the true labels corresponding to each input
+           y_pred {numpy.array} -- the model's predictions
 
+       Returns:
+           confusion_matrix {numpy.array} -- the calculated confusion matrix
+    """
+    return confusion_matrix(y_true, y_pred)
 
-# classification report using scikit-learn's API
-print("Test Set: \n" + classification_report(test_labels, test_preds))
-print("All Data: \n" + classification_report(test_labels_all, test_preds_all))
+def calculate_mmc(y_true, y_pred):
+    """Using sklearn to compute the Matthews correlation coefficient (MCC)
 
+       Arguments:
+           y_true {numpy.array} -- the true labels corresponding to each input
+           y_pred {numpy.array} -- the model's predictions
 
-# calculating the matthew's correlation coefficient
-print(matthews_corrcoef(test_labels_all, test_preds_all))
+       Returns:
+           mmc {float} -- the calculated Matthews correlation coefficient
+    """
+    return matthews_corrcoef(y_true, y_pred)
+    
